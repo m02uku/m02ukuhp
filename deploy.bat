921d91810337a1cac5fb802f2708f61e91c6e688
m02uku <m02ukuzrzr@gmail.com>
2024-04-15 17:10:04 +0900

    [UPDATE]

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,5 @@
-git add -A
-git commit -am "[UPDATE]"
-git push -u origin main
 npm run build
-git checkout gh-pages
-git merge main
 git add -A
-git commit -am "[BUILD]"
+git commit -m "[UPDATE]"
+git push -u origin main
 git subtree push --prefix=build/ origin gh-pages
-git checkout main
-git branch
