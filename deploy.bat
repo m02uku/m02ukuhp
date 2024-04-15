3246700034c5f48d3d2382cfafa5a3c0c92c55c3
m02uku <157340743+m02uku@users.noreply.github.com>
2024-04-16 08:52:10 +0900

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -7,5 +7,10 @@ echo ========================================
 echo Commit message: %~1
 echo ========================================
 git commit -m %1
-git push -f origin main
+echo Commit message: %~1
+
+git config pull.rebase false
+git pull origin main
+git merge --allow-unrelated-histories origin/main
+git push -u origin main
 git subtree push --prefix=build/ origin gh-pages
