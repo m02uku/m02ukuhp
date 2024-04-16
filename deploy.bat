2dca5dd40126ca8629a33b6afdf0ccea7c098515
m02uku <157340743+m02uku@users.noreply.github.com>
2024-04-16 09:46:44 +0900

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -3,12 +3,9 @@ cd %~dp0
 
 call npm run build
 git add -A
-echo ========================================
-echo Commit message: %~1
-echo ========================================
 git commit -m %1
 
 git config pull.rebase false
-git merge --allow-unrelated-histories origin/main
+git pull --allow-unrelated-histories origin main
 git push -u origin main
 git subtree push --prefix=build/ origin gh-pages
