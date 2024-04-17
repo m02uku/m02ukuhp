3d3d8bf9a58721c0d056ffb2b356b4948f87288f
m02uku <m02ukuzrzr@gmail.com>
2024-04-18 00:19:36 +0900

    [SOLVED] route

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -1,5 +1,12 @@
+import Sidebar from "../components/Sidebar";
+
 const Links = () => {
-    return <h1>This is link page.</h1>;
+    return (
+        <>
+            <Sidebar />
+            <h1>This is link page.</h1>
+        </>
+    );
 };
 
 export default Links;
